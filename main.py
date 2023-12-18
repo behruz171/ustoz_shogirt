3ca3275bfc767b00b154b4b7ef8af4917c548e25
behruz171 <behruzzo662@gmail.com>
2023-12-18 19:01:11 +0500

    first comment

---
+++ main.py
@@ -0,0 +1,16 @@
+from aiogram import types, Dispatcher, executor, Bot
+from dotenv import load_dotenv
+import os
+load_dotenv()
+bot_token = os.getenv("BOT_TOKEN")
+bot = Bot(token=bot_token)
+
+dp = Dispatcher(bot)
+
+async def on_startup(_):
+    print("Bot ishladi")
+
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
